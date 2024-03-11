bfde38d39b62010c206b194f4e1a857c74c941d8
Sergey Chernikov <sergey.chernikov@sysgears.com>
2024-03-11 13:20:47 +0200

    Added star input change listener

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [starInput, setStarInput] = useState("");
+
+  const starInputChangeHandler = (enteredText) => {
+    setStarInput(enteredText);
+  };
+
+  const addStarBtnPressHandler = () => {
+    console.log("Star added: ", starInput);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Add a name of the star"
+          onChangeText={starInputChangeHandler}
         />
-        <Button title="Add star" />
+        <Button title="Add star" onPress={addStarBtnPressHandler} />
       </View>
       <View style={styles.starsContainer}>
         <Text>List of stars...</Text>
