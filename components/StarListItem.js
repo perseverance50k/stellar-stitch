b35ee903b5d661790593c617fe767baed641125e
Sergey Chernikov <sergey.chernikov@sysgears.com>
2024-03-11 16:02:21 +0200

    Added on-press visual effects for stars list items (for Android and iOS alternative)

--- components/StarListItem.js
+++ components/StarListItem.js
@@ -2,15 +2,17 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export const StarListItem = ({ item, onItemPress }) => {
   return (
-    <Pressable
-      onPress={() => {
-        onItemPress(item.id);
-      }}
-    >
-      <View style={styles.starsListItem}>
+    <View style={styles.starsListItem}>
+      <Pressable
+        android_ripple={{ color: "#ffd500" }}
+        onPress={() => {
+          onItemPress(item.id);
+        }}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.starsListItemText}>{item.value}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -18,10 +20,13 @@ const styles = StyleSheet.create({
   starsListItem: {
     backgroundColor: "#ff8c00",
     margin: 3,
-    padding: 10,
     borderRadius: 10,
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   starsListItemText: {
+    padding: 10,
     color: "#ffffff",
     fontWeight: "600",
   },
